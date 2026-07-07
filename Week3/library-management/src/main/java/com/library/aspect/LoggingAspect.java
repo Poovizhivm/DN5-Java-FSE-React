3399ea0a630cc1fa20d156a6aeb15a89d3b95c7d
Poovizhivm <poovizhivm@gmail.com>
2026-07-07 14:29:27 +0530

    Week 3 Exercise 8 - Annotation Based Spring AOP

--- Week3/library-management/src/main/java/com/library/aspect/LoggingAspect.java
+++ Week3/library-management/src/main/java/com/library/aspect/LoggingAspect.java
@@ -1,11 +1,20 @@
 package com.library.aspect;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
 
+    @Before("execution(* com.library.service.BookService.*(..))")
     public void beforeMethod() {
         System.out.println("LoggingAspect: Method execution started...");
     }
 
+    @After("execution(* com.library.service.BookService.*(..))")
     public void afterMethod() {
         System.out.println("LoggingAspect: Method execution completed...");
     }
